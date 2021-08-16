a9737b2c99bf176b17d72925c807b2f810d40c49
pendos <djvlad967891@gmail.com>
2021-08-16 14:55:21 +0300

    fix big

--- firmware/pump_encoder/pump_encoder.ino
+++ firmware/pump_encoder/pump_encoder.ino
@@ -1,5 +1,5 @@
-#define pressure_port 0
-#define relay_port 5
+#define pressure_Aport 0
+#define relay_Dport 5
 
 #define CLK 2
 #define DT 3
@@ -21,34 +21,33 @@ Encoder enc(CLK, DT, SW);
 void setup() {
   Serial.begin(9600);
   enc.setType(TYPE2);
-
+  pinMode(relay_Dport, OUTPUT);
 }
 
 void loop() {
 
-  // encoder_val = constrain(map(analogRead(encoder_port), 0, 1023, const_pressure_min, const_pressure_max), const_pressure_max);
 
   enc.tick();
   if (enc.isTurn()) {
     if (enc.isRight()) {
-      pressure_min = constrain(pressure_min++, pressure_min, const_pressure_max);
+      pressure_min = constrain(pressure_min+10, pressure_min, const_pressure_max);
     }
     if (enc.isLeft()) {
-      pressure_min = constrain(pressure_min--, pressure_min, const_pressure_max);
+      pressure_min = constrain(pressure_min-10, pressure_min, const_pressure_max);
     }
     if (enc.isRightH()) {
-      pressure_min = constrain(pressure_max++, pressure_min, const_pressure_max);
+      pressure_min = constrain(pressure_max+10, pressure_min, const_pressure_max);
     }
     if (enc.isLeftH()) {
-      pressure_min = constrain(pressure_max--, pressure_min, const_pressure_max);
+      pressure_min = constrain(pressure_max-10, pressure_min, const_pressure_max);
     }
   }
   
-  pressure = analogRead(pressure_port);
+  pressure = analogRead(pressure_Aport);
 
   if (pressure < pressure_min) {
     if (!flag) {
-      digitalWrite(relay_port, HIGH);
+      digitalWrite(relay_Dport, HIGH);
       flag = true;
     }
     
@@ -56,7 +55,7 @@ void loop() {
   } else {
       if (pressure > pressure_max) {
         if (flag) {
-          digitalWrite(relay_port, LOW);
+          digitalWrite(relay_Dport, LOW);
           flag = false;
         }
         
