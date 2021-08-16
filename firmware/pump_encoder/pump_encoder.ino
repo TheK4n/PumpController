fc80e42859e9c56cc9700531a620be7fb4673132
pendos <djvlad967891@gmail.com>
2021-08-16 15:33:22 +0300

    add eeprom

--- firmware/pump_encoder/pump_encoder.ino
+++ firmware/pump_encoder/pump_encoder.ino
@@ -6,10 +6,10 @@
 #define SW 4
 
 #define const_pressure_min 0
-#define const_pressure_max 100
+#define const_pressure_max 1000
 
 #include <GyverEncoder.h>
-
+#include <EEPROM.h>
 
 
 int pressure, pressure_min, pressure_max;
@@ -22,6 +22,8 @@ void setup() {
   Serial.begin(9600);
   enc.setType(TYPE2);
   pinMode(relay_Dport, OUTPUT);
+  EEPROM.get(0, pressure_min);
+  EEPROM.get(2, pressure_max);
 }
 
 void loop() {
@@ -30,18 +32,24 @@ void loop() {
   enc.tick();
   if (enc.isTurn()) {
     if (enc.isRight()) {
-      pressure_min = constrain(pressure_min+10, pressure_min, const_pressure_max);
+      pressure_min = constrain(pressure_min+10, const_pressure_min, const_pressure_max);
     }
     if (enc.isLeft()) {
-      pressure_min = constrain(pressure_min-10, pressure_min, const_pressure_max);
+      pressure_min = constrain(pressure_min-10, const_pressure_min, const_pressure_max);
     }
     if (enc.isRightH()) {
-      pressure_min = constrain(pressure_max+10, pressure_min, const_pressure_max);
+      pressure_min = constrain(pressure_max+10, const_pressure_min, const_pressure_max);
     }
     if (enc.isLeftH()) {
-      pressure_min = constrain(pressure_max-10, pressure_min, const_pressure_max);
+      pressure_min = constrain(pressure_max-10, const_pressure_min, const_pressure_max);
     }
   }
+
+  if (enc.isClick()) {
+    EEPROM.put(0, pressure_min);
+    EEPROM.put(2, pressure_max);
+  }
+
   
   pressure = analogRead(pressure_Aport);
 
